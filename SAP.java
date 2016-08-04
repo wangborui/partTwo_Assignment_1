6a2f13b3989412c90a08333bdb7bb272d0cec76e
Borui Wang <wangbd@bc.edu>
2016-08-03 20:33:59 -0500

    Initializing SAP class

---
+++ SAP.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partTwo_Assignment_1;
+
+import edu.princeton.cs.algs4.Digraph;
+
+/**
+ *
+ * @author Borui Wang
+ */
+public class SAP {
+
+   // constructor takes a digraph (not necessarily a DAG)
+   public SAP(Digraph G)
+   {
+       
+   }
+
+   // length of shortest ancestral path between v and w; -1 if no such path
+   public int length(int v, int w)
+   {
+       return -1;
+   }
+
+   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+   public int ancestor(int v, int w)
+   {
+       return -1;
+   }
+
+   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+   public int length(Iterable<Integer> v, Iterable<Integer> w)
+   {
+       return -1;
+   }
+
+   // a common ancestor that participates in shortest ancestral path; -1 if no such path
+   public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
+   {
+       return -1;
+   }
+
+   // do unit testing of this class
+   public static void main(String[] args){
+       
+   }
+}
