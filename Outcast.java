31c0fc0b7f2ce8c94188b482c3073c71e4bc29c5
Borui Wang <wangbd@bc.edu>
2016-08-07 20:21:01 -0500

    added outcast class

---
+++ Outcast.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partTwo_Assignment_1;
+
+/**
+ *
+ * @author Borui Wang
+ */
+public class Outcast {
+   public Outcast(WordNet wordnet)         // constructor takes a WordNet object
+   {
+       
+   }
+   public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
+   {
+       return null;
+   }
+   public static void main(String[] args)  // see test client below
+   {
+       
+   }
+}
